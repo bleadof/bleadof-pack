977ea9111133dd9896a5841a1f9310e38c833d74
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2013-04-30 10:46:05 +0300

    Made coffee mode tab-width 2

--- config/coffee-mode-conf.el
+++ config/coffee-mode-conf.el
@@ -1,3 +1,6 @@
 (live-add-pack-lib "coffee-mode")
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-hook 'coffee-mode-hook
+          (lambda ()
+            (setq tab-width 2)))
