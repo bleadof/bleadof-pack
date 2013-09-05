59f1c656b727f996f45124f1ce8301a1eed0eb7e
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2013-09-05 17:20:46 +0300

    Associate .jst with html-mode

--- init.el
+++ init.el
@@ -3,9 +3,9 @@
 ;; User this file to initiate the pack configuration.
 ;; See README for more information.
 
-;; Load bindings config
 (live-load-config-file "mac-keys.el")
 (live-load-config-file "auto-complete-conf.el")
+(live-load-config-file "associations.el")
 (live-load-config-file "bindings.el")
 (live-load-config-file "indent-all-conf.el")
 (live-load-config-file "less-css-mode-conf.el")
