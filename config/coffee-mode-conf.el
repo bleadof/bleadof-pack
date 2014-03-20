984627b076be81103f41bc6181935b0adc3c694e
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2014-03-20 14:30:01 +0200

    Add autocompletion to coffee-mode

--- config/coffee-mode-conf.el
+++ config/coffee-mode-conf.el
@@ -1,6 +1,7 @@
 (live-add-pack-lib "coffee-mode")
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'ac-modes 'coffee-mode)
 (add-hook 'coffee-mode-hook
           (lambda ()
             (setq tab-width 2)))
