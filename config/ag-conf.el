d9a4947f352899739201866767d9a09f1996fbb9
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2014-03-20 15:31:51 +0200

    Add ag.el integration

---
+++ config/ag-conf.el
@@ -0,0 +1,2 @@
+(live-add-pack-lib "ag.el")
+(require 'ag)
--- init.el
+++ init.el
@@ -8,7 +8,14 @@
 (live-load-config-file "auto-complete-conf.el")
 (live-load-config-file "associations.el")
 (live-load-config-file "bindings.el")
+
+;; Libs
 (live-load-config-file "indent-all-conf.el")
+
+;; Utility integration
+(live-load-config-file "ag-conf.el")
+
+;; Modes
 (live-load-config-file "less-css-mode-conf.el")
 (live-load-config-file "coffee-mode-conf.el")
 (live-load-config-file "php-mode-conf.el")
