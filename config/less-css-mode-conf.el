1a833f26f0312ecadcda1c27b4ae30c2069e6d94
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2014-03-20 17:15:22 +0200

    Setup less css mode indent

--- config/less-css-mode-conf.el
+++ config/less-css-mode-conf.el
@@ -2,3 +2,6 @@
 (require 'less-css-mode)
 (autoload 'less-css-mode "less-css-mode" "Major mode for less css." t)
 (add-to-list 'auto-mode-alist '("\\.less$" . less-css-mode))
+(add-hook 'less-css-mode-hook
+          (lambda ()
+            (setq css-indent-offset 2)))
